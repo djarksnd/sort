d9f1693a4cc937c7678f5ae296576e506c2f207a
엄관웅 <djarksnd@gmail.com>
2018-09-26 02:26:54 +0900

    나즈나쨩 미츠하쨩 까와이데쓰나이트

--- sort.h
+++ sort.h
@@ -112,192 +112,114 @@ namespace sort
 	}
 
 	template<typename RandomAccessIterator, typename Compare>
-	void _dualPivotQuickSort(RandomAccessIterator first, RandomAccessIterator last, const Compare& cmp)
+	inline RandomAccessIterator _guessPivot(const RandomAccessIterator& first, const RandomAccessIterator& last, const Compare& cmp)
 	{
-		while (first < last)
+		const auto count = last - first;
+		if (40 < count)
 		{
-			auto count = last - first;
+			const auto offset = (count + 1) / 10;
+			const RandomAccessIterator samples[11] = {
+				first,
+				first + offset,
+				first + (offset * 2),
+				first + (offset * 3),
+				first + (offset * 4),
+				first + (offset * 5),
+				first + (offset * 6),
+				first + (offset * 7),
+				first + (offset * 8),
+				first + (offset * 9),
+				last - 1 };
 
-			if (count > 32)
+			for (size_t i = 1; i < 11; ++i)
 			{
-				RandomAccessIterator pivot1;
-				RandomAccessIterator pivot2;
-
-				if (count > 40)
+				for (size_t j = i; j > 0; --j)
 				{
-					const size_t numSampes = 14;
-					const auto offset = (count + 1) / (numSampes - 1);
-				
-					RandomAccessIterator samples[numSampes] = {
-						first,
-						first + offset,
-						first + (offset * 2),
-						first + (offset * 3),
-						first + (offset * 4),
-						first + (offset * 5),
-						first + (offset * 6),
-						first + (offset * 7),
-						first + (offset * 8),
-						first + (offset * 9),
-						first + (offset * 10),
-						first + (offset * 11),
-						first + (offset * 12),
-						last - 1};
-
-					for (size_t i = 1; i < numSampes; ++i)
-					{
-						for (size_t j = i; j > 0; --j)
-						{
-							if (!cmp(*(samples[j]), *(samples[j - 1])))
-								break;
-
-							sort::swap(*(samples[j]), *(samples[j - 1]));
-						}
-					}
-
-					pivot1 = samples[4];
-					pivot2 = samples[9];
-				}
-				else
-				{
-					const auto offset = (count + 1) >> 2;
-					pivot1 = first + offset;
-					pivot2 = pivot1 + offset + offset;
-
-					sort::_sort5(first, pivot1, pivot1 + offset, pivot2, last - 1, cmp);
-				}
-
-				sort::swap(*pivot1, *first);
-				sort::swap(*pivot2, *(last - 1));
-
-				pivot1 = first;
-				pivot2 = last - 1;
-
-				RandomAccessIterator less = pivot1 + 1;
-				RandomAccessIterator great = pivot2 - 1;
-
-				for (RandomAccessIterator i = less; i <= great; ++i)
-				{
-					if (cmp(*i, *pivot1))
-					{
-						sort::swap(*i, *less);
-						++less;
-					}
-					else if (!cmp(*i, *pivot2))
-					{
-						while (cmp(*pivot2, *great) && i < great)
-						{
-							--great;
-						}
-
-						sort::swap(*i, *great);
-						--great;
-
-						if (cmp(*i, *pivot1))
-						{
-							sort::swap(*i, *less);
-							++less;
-						}
-					}
-				}
-
-				--less;
-				++great;
-
-				sort::swap(*pivot1, *less);
-				sort::swap(*pivot2, *great);
-
-				pivot1 = less;
-				pivot2 = great;
-
-				for (RandomAccessIterator i = less + 1; i < great; ++i)
-				{
-					if (cmp(*less, *i))
+					if (!cmp(*(samples[j]), *(samples[j - 1])))
 						break;
 
-					++pivot1;
-				}
-
-				for (RandomAccessIterator i = great + 1; i < last; ++i)
-				{
-					if (cmp(*great, *i))
-						break;
-
-					++pivot2;
-				}
-
-				const auto count1 = less - first;
-				const auto count2 = great - (pivot1 + 1);
-				const auto count3 = last - (pivot2 + 1);
-
-				if (count1 > count2)
-				{
-					sort::_dualPivotQuickSort(pivot1 + 1, great, cmp);
-
-					if (count3 > count1)
-					{
-						sort::_dualPivotQuickSort(first, less, cmp);
-						first = pivot2 + 1;
-					}
-					else
-					{
-						sort::_dualPivotQuickSort(pivot2 + 1, last, cmp);
-						last = less;
-					}
-				}
-				else
-				{
-					sort::_dualPivotQuickSort(first, less, cmp);
-
-					if (count3 > count2)
-					{
-						sort::_dualPivotQuickSort(pivot1 + 1, great, cmp);
-						first = pivot2 + 1;
-					}
-					else
-					{
-						sort::_dualPivotQuickSort(pivot2 + 1, last, cmp);
-						first = pivot1 + 1;
-						last = great;
-					}
+					sort::swap(*(samples[j]), *(samples[j - 1]));
 				}
 			}
-			else if (count > 5)
+
+			return samples[5];
+		}
+		else
+		{
+			const RandomAccessIterator middle = first + ((last - first) >> 1);
+			const auto step = (count + 1) >> 2;
+			const auto lastMinusOne = last - 1;
+			sort::_sort5(first, first + step, middle, lastMinusOne - step, lastMinusOne, cmp);
+
+			return middle;
+		}
+	}
+
+	template<typename RandomAccessIterator, typename Compare>
+	inline void _partition(RandomAccessIterator& first, RandomAccessIterator& last, const Compare& cmp, RandomAccessIterator& outSecond, RandomAccessIterator& outThird)
+	{
+		RandomAccessIterator pivot = sort::_guessPivot(first, last, cmp);
+		sort::swap(*pivot, *(last - 1));
+		pivot = last - 1;
+
+		outSecond = first;
+
+		for (RandomAccessIterator i = first; i < pivot; ++i)
+		{
+			if (cmp(*i, *pivot))
 			{
-				sort::insertionSort(first, last, cmp);
-				return;
+				sort::swap(*i, *outSecond);
+				++outSecond;
 			}
-			else if (count == 5)
+		}
+
+		sort::swap(*pivot, *outSecond);
+
+		outThird = outSecond + 1;
+		for (RandomAccessIterator i = outThird; i < last; ++i)
+		{
+			if (!cmp(*outSecond, *i))
 			{
-				sort::_sort5(first, first + 1, first + 2, first + 3, first + 4, cmp);
-				return;
-			}
-			else if (count == 4)
-			{
-				sort::_sort4(first, first + 1, first + 2, first + 3, cmp);
-				return;
-			}
-			else if (count == 3)
-			{
-				sort::_sort3(first, first + 1, first + 2, cmp);
-				return;
-			}
-			else if (count == 2)
-			{
-				if (cmp(*(first + 1), *first))
-					sort::swap(*(first + 1), *first);
-				return;
-			}
-			else
-			{
-				return;
+				std::swap(*i, *(outThird));
+				++outThird;
 			}
 		}
 	}
 
 	template<typename RandomAccessIterator, typename Compare>
-	void sort(const RandomAccessIterator& first, const RandomAccessIterator& last, const Compare& cmp)
+	inline void _quickSort(RandomAccessIterator first, RandomAccessIterator last, const Compare& cmp)
 	{
-		sort::_dualPivotQuickSort(first, last, cmp);
+	quickSortLoopStart:
+		auto count = last - first;
+
+		if (count > 32)
+		{
+			RandomAccessIterator second;
+			RandomAccessIterator third;
+			sort::_partition(first, last, cmp, second, third);
+
+			if (last - third > second - first)
+			{
+				sort::_quickSort(first, second, cmp);
+				first = third;
+			}
+			else
+			{
+				sort::_quickSort(third, last, cmp);
+				last = second;
+			}
+
+			goto quickSortLoopStart;
+		}
+		else if (count > 1)
+		{
+			insertionSort(first, last, cmp);
+		}
+	}
+
+	template<typename RandomAccessIterator, typename Compare>
+	inline void sort(const RandomAccessIterator& first, const RandomAccessIterator& last, const Compare& cmp)
+	{
+		sort::_quickSort(first, last, cmp);
 	}
 }
